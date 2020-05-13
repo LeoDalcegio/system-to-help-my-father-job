f0fe8d7b832d5174ab48f6e036ea429495155825
Leonardo Luís Dalcegio <leoodalcegio@hotmail.com>
2020-05-13 16:27:48 +0000

    index da movimentacao

--- server/src/controllers/MovimentacaoController.js
+++ server/src/controllers/MovimentacaoController.js
@@ -10,7 +10,26 @@ module.exports = {
      * @return {[JSON]} JSON contendo todas as movimentações
      */
     async index(request, response) {
+        const { page = 1, limit = 10, tipo } = request.query;
         
+        try {
+            const movimentacoes = await connection('movimentacao')
+                .join('cliente', 'cliente.id', '=', 'movimentacao.cliente_id')
+                .join('produto', 'produto.id', '=', 'movimentacao.produto_id')
+                .where('movimentacao.tipo', tipo)
+                .limit(limit)
+                .offset((page - 1) * 5)
+                .select(
+                    'movimentacao.*', 
+                    'cliente.nome', 
+                    'produto.codigo_do_produto', 
+                    'produto.descricao_do_produto'
+                );
+
+            return response.json(movimentacoes);
+        } catch (err) {
+            return response.status(400).send(err);
+        }
     },
 
     /**
@@ -105,7 +124,7 @@ module.exports = {
      */
     async destroy(request, response) {
         const { id } = request.params;
-        
+
         try {
             await connection('movimentacao')
                 .where('id', id)

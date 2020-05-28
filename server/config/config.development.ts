c0844dfa1a2c8de908316e0f420343f63230514e
Leonardo Luís Dalcegio <leoodalcegio@hotmail.com>
2020-05-28 20:12:35 +0000

    changed connection string

--- server/config/config.development.ts
+++ server/config/config.development.ts
@@ -1,6 +1,6 @@
 import { Dialect } from 'sequelize/types';
 
 export const config = {
-    database: '	postgres://nsnppava:p20opkKQZMaodEYfYTuwncJxHnC5K46g@tuffi.db.elephantsql.com:5432/nsnppava',
+    database: 'postgres://nsnppava:p20opkKQZMaodEYfYTuwncJxHnC5K46g@tuffi.db.elephantsql.com:5432/nsnppava',
     jwtPrivateKey: 'jwtPrivateKey',
 };

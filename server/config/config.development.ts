d4abfed761f0b07f13977d2a6431d4f1147d7155
Leonardo Luís Dalcegio <leoodalcegio@hotmail.com>
2020-05-28 10:53:19 +0000

    changed db to heroku

--- server/config/config.development.ts
+++ server/config/config.development.ts
@@ -3,11 +3,11 @@ import { Dialect } from 'sequelize/types';
 export const config = {
     database: {
         dialect: 'postgres' as Dialect,
-        host: '127.0.0.1',
-        port: 56537,
-        username: 'postgres',
-        password: 'postgres',
-        database: 'tecelagem',
+        host: 'ec2-18-210-214-86.compute-1.amazonaws.com',
+        port: 5432,
+        username: 'gqtjxczsygmzdl',
+        password: 'c7676aa249318a88c76d8ffad12b7ade59aec52673f7af4e7739cf6406b650d5',
+        database: 'dfvdoaqo719rvq',
         logging: false,
     },
     jwtPrivateKey: 'jwtPrivateKey',

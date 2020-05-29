bc63b040c40d70305f3aefd76377d0965b980162
Leonardo Luís Dalcegio <leoodalcegio@hotmail.com>
2020-05-29 12:39:17 +0000

    database up and running

--- server/config/config.development.ts
+++ server/config/config.development.ts
@@ -1,6 +1,6 @@
 import { Dialect } from 'sequelize/types';
 
 export const config = {
-    database: 'postgres://nsnppava:p20opkKQZMaodEYfYTuwncJxHnC5K46g@tuffi.db.elephantsql.com:5432/nsnppava',
+    database: 'postgres://gqtjxczsygmzdl:c7676aa249318a88c76d8ffad12b7ade59aec52673f7af4e7739cf6406b650d5@ec2-18-210-214-86.compute-1.amazonaws.com:5432/dfvdoaqo719rvq?ssl=true',
     jwtPrivateKey: 'jwtPrivateKey',
 };
--- server/src/main.ts
+++ server/src/main.ts
@@ -4,9 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './swagger';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-
     const PORT = 3000 || process.env.PORT;
+
+    const app = await NestFactory.create(AppModule);
 
     app.useGlobalPipes(new ValidationPipe());
 
